d55625b9a09a7c8b2afd1980063b74115c2a6302
Matt-Chee123 <cheematthew10@gmail.com>
2026-01-15 19:07:29 +0000

    initial setup and config

---
+++ config/config.py
@@ -0,0 +1,20 @@
+from datetime import date
+
+TICKERS = [
+    'PG',    # Procter & Gamble
+    'KO',    # Coca-Cola
+    'WMT',   # Walmart
+    'COST',  # Costco
+    'JNJ',   # Johnson & Johnson
+    'UNH',   # UnitedHealth
+    'PFE',   # Pfizer
+    'MRK'    # Merck
+]
+
+START_DATE = '2021-01-01'
+END_DATE = '2026-15-01'
+
+RAW_DATA_PATH = '../data/raw'
+PROCESSED_DATA_PATH = '../data/processed'
+
+INTERVAL = '1d'
