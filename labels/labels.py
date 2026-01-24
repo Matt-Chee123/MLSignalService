f4e16c619dffb5e59f87bb6dd7beb696894e60b6
Matt-Chee123 <cheematthew10@gmail.com>
2026-01-24 15:33:32 +0000

    create label generator class

--- labels/labels.py
+++ labels/labels.py
@@ -6,10 +6,16 @@ class LabelGenerator:
         self.horizon = horizon
 
     def add_return_label(self, df):
-        pass
+        df['return_label'] = df.groupby('ticker')['Close'].shift(-self.horizon) / df['Close'] - 1
+        return df
 
     def add_binary_label(self, df):
-        pass
+        pct_change = df.groupby('ticker')['Close'].shift(-self.horizon) / df['Close'] - 1
+        df['binary_label'] = (pct_change > 0).astype(float).where(pct_change.notna())
+        return df
 
     def add_relative_rank(self, df):
-        pass
+        df = self.add_return_label(df)
+        df['rank_label'] = df.groupby(level='date')['return_label'].rank(pct=True)
+        df = df.drop(['return_label'], axis=1)
+        return df
