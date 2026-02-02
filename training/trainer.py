cf3dbe76554cd8b83625626802c2364dcbca5fe9
Matt-Chee123 <cheematthew10@gmail.com>
2026-02-02 21:19:40 +0000

    basic trainer framework

---
+++ training/trainer.py
@@ -0,0 +1,27 @@
+from models.model_factory import get_model_from_config
+from sklearn.metrics import mean_squared_error, r2_score
+import joblib
+from pathlib import Path
+
+class Trainer:
+    def __init__(self, model_config, output_dir="./models/"):
+        #model config like {'model_type': things, 'hyperparams': {}}
+        self.model_config = model_config
+        self.model = get_model_from_config(model_config)
+        self.output_dir = Path(output_dir)
+
+    def fit(self, X_train, y_train):
+        self.model.fit(X_train, y_train)
+        return self
+
+    def predict(self, X_test):
+        return self.model.predict(X_test)
+
+    def evaluate(self, X_test, y_test):
+        y_preds = self.predict(X_test)
+        mse = mean_squared_error(y_test, y_preds)
+        r2 = r2_score(y_test, y_preds)
+        return {'mse': mse, 'r2': r2}
+
+    def save_model(self, name='model.pkl'):
+        joblib.dump(self.model, self.output_dir / name)
