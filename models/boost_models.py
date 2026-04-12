fa0ea13ad39c3cf4a159112ee3395e5e1a1a3bb9
Matt-Chee123 <cheematthew10@gmail.com>
2026-04-12 09:51:39 +0100

    add in xgb

---
+++ models/boost_models.py
@@ -0,0 +1,24 @@
+from xgboost import XGBRegressor
+from .base_model import BaseModel
+
+class XgBoostModel(BaseModel):
+    def create_model(self):
+        self.model = XGBRegressor(
+            n_estimators=self.hyperparameters.get('n_estimators', 300),
+            learning_rate=self.hyperparameters.get('learning_rate', 0.05),
+            max_depth=self.hyperparameters.get('max_depth', 6),
+
+            subsample=self.hyperparameters.get('subsample', 0.8),
+            colsample_bytree=self.hyperparameters.get('colsample_bytree', 0.8),
+
+            min_child_weight=self.hyperparameters.get('min_child_weight', 1),
+            gamma=self.hyperparameters.get('gamma', 0),
+
+            reg_alpha=self.hyperparameters.get('reg_alpha', 0),
+            reg_lambda=self.hyperparameters.get('reg_lambda', 1),
+
+            random_state=self.hyperparameters.get('random_state', 42),
+            n_jobs=self.hyperparameters.get('n_jobs', -1),
+
+            objective=self.hyperparameters.get('objective', 'reg:squarederror')
+        )
--- models/model_factory.py
+++ models/model_factory.py
@@ -1,4 +1,5 @@
 from .sklearn_models import RandomForestModel, GradientBoostingModel
+from .boost_models import XgBoostModel
 
 def get_model_from_config(model_config):
     model_type = model_config.get('model_type').lower()
@@ -8,5 +9,7 @@ def get_model_from_config(model_config):
         return RandomForestModel("Random Forest", hyperparams)
     elif model_type == 'gradient_boosting':
         return GradientBoostingModel("Gradient Boosting", hyperparams)
+    elif model_type == 'xgboost':
+        return XgBoostModel("XGBoost", hyperparams)
     else:
         raise ValueError(f"Unknown model type: {model_type}")
